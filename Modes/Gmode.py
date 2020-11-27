ec1ee5b12e8640f7397d06b9aa53ff41cdaa7043
eno leriand <59614781+enoleriand-alt@users.noreply.github.com>
2020-11-28 00:17:29 +0900

    Update Gmode.py

--- Modes/Gmode.py
+++ Modes/Gmode.py
@@ -8,7 +8,7 @@ import warnings
 import random
 from http import cookiejar
 class BlockAll(cookiejar.CookiePolicy):
-    return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
+    return_ok = set_ok = domain_return_ok = path_return_ok = la>
     netscape = True
     rfc2965 = hide_cookie2 = False
 
@@ -25,27 +25,24 @@ def clear():
 print ("")
 
 A = """
-
-
- ██████╗  ██████╗  ██████╗  ██████╗ ██╗     ███████╗
-██╔════╝ ██╔═══██╗██╔═══██╗██╔════╝ ██║     ██╔════╝
-██║  ███╗██║   ██║██║   ██║██║  ███╗██║     █████╗  
-██║   ██║██║   ██║██║   ██║██║   ██║██║     ██╔══╝  
-╚██████╔╝╚██████╔╝╚██████╔╝╚██████╔╝███████╗███████╗
- ╚═════╝  ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝╚══════╝
-                                                    
+    ^v^h   ^v^h   ^v^h   ^v^h   ^v^h   ^v^h   ^u^w     ^v^h   ^>
+   ^v^h   ^v^h   ^u^t   ^u^p   ^u^p   ^u^p   ^u^p   ^u^}    ^v^>
+   ^v^h   ^v^h   ^u^q     ^v^h   ^v^h   ^v^h   ^u^w   ^v^h   ^v>
+   ^v^h   ^v^h   ^u^q      ^v^h   ^v^h   ^u^q   ^v^h   ^v^h   ^>
+   ^u^z   ^v^h   ^v^h   ^v^h   ^v^h   ^v^h   ^v^h   ^u^t   ^u^}>
+    ^u^z   ^u^p   ^u^p   ^u^p   ^u^p   ^u^p   ^u^}     ^u^z   ^>
 
 """
 print ("")
 print(A)
-TLD = ["com","com.tw","co.in","be","de","co.uk","co.ma","dz","ru","ca"]
+TLD = ["go.id","com","com.tw","co.in","be","de","co.uk","co.ma">
 s = requests.Session()
 s.cookies.set_policy(BlockAll())
-alpha = input (colored('[>] Please set a Dork : ', 'green' ))
+alpha = input (colored('> Masukkan Dork  : ', 'blue' ))
 query = alpha
 beta =  random.choice(TLD)
 
-for gamma in search(query, tld=beta, num=10 , stop=95 , pause=2): 
-    print(colored ('[+] Found > ' ,'yellow')  + (gamma) )
-print(colored ('[+] Done  ' ,'green'))
-print(colored ('[! >] delete .google-cookie file in casper DIR  ' ,'red')) 
+for gamma in search(query, tld=beta, num=10 , stop=95 , pause=2>
+    print(colored ('==> Joss > ' ,'red')  + (gamma) )
+print(colored ('=/> Selesai  ' ,'green'))
+print(colored ('[! >] Mengahapus .google-cookie Otomatis  ' ,'b>
